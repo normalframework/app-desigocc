b3543d4aa92ba61915758337546340d9e67556fe
normaljosh <josh@normal.dev>
2023-07-05 12:58:14 -0400

    Add Desigo CC App for NORIS data aquisition

---
+++ hooks/import-points.js
@@ -0,0 +1,93 @@
+const NormalSdk = require("@normalframework/applications-sdk");
+const { v5: uuidv5 } = require("uuid");
+
+
+const batch_size = 100
+
+const NAMESPACE = "fe927c12-7f2f-11ee-a65f-af8737c274cc"
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+ 
+
+/**
+ * Invoke hook function
+ * @param {NormalSdk.InvokeParams} params
+ * @returns {NormalSdk.InvokeResult}
+ */
+module.exports = async ({sdk, config}) => {
+  http = sdk.http;
+
+  if (!config.username || !config.password || !config.baseUrl) {
+    return NormalSdk.InvokeError("missing username, password, or base url")
+  }
+  config.baseUrl = config.baseUrl.replace(/\/+$/gm, '')
+  console.log(config)
+  const { data } = await http.post(config.baseUrl + "/token", {
+    grant_type: "password",
+    "username": config.username,
+    "password": config.password,
+    }, {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+          timeout: 15000,
+
+  })
+
+  if (!config.viewId || !config.systemId) {
+    sdk.event("Desigo view ID is not configured.  Loading the available system views!")
+    let views = await http.get(config.baseUrl+"/systembrowser", {
+      headers: {
+        "authorization": "Bearer " + data.access_token
+    },
+    })
+    for (let i = 0; i < views.data.length; i ++) {
+      sdk.event(`view systemId=${views.data[i].SystemId} viewId=${views.data[i].ViewId} descriptor=${views.data[i].Descriptor} designation=${views.data[i].Designation}`)
+    }
+    sdk.event("Please select a systemId and viewId and update the configuration.")
+    return
+  }
+  let total = 0
+  let added = 0
+
+  for (let page = 0; page < 1000; page ++) { 
+    let views = await http.get(config.baseUrl+`/systembrowser/${config.systemId}/${config.viewId}?size=${batch_size}&page=${page}&searchString=*&diciplineFilter=50`, {
+      headers: {
+        "authorization": "Bearer " + data.access_token
+      },
+      timeout: 15000,
+    })
+
+    let nodes = views.data.Nodes
+    let points = []
+    if (nodes.length == 0) {
+      break
+    }
+    for (let i = 0; i < nodes.length; i++) {
+      console.log(nodes[i].ObjectId)
+      if (nodes[i].Attributes.DefaultProperty == "Present_Value") {
+        points.push({
+          layer: "hpl:desigocc",
+          uuid: uuidv5(nodes[i].ObjectId, NAMESPACE),
+          name: nodes[i].Name,
+          attrs: {
+            "objectId": nodes[i].ObjectId,
+            "designation": nodes[i].Designation,
+            "managedTypeName": nodes[i].Attributes.ManagedTypeName,
+            "objectModelName": nodes[i].Attributes.ObjectModelName,
+          },
+          protocol_id: nodes[i].ObjectId,
+          point_type: "POINT",
+        })
+      }
+    }
+    sdk.event(`Adding ${points.length} points`)
+    added += points.length
+    total += nodes.length
+    let res = await http.post("http://localhost:8080/api/v1/point/points", {
+      "points": points,
+    })
+  }
+  sdk.event(`finsihed import. ${added}/${total} imported`)
+};
---
+++ hooks/trend-data.js
@@ -0,0 +1,86 @@
+const NormalSdk = require("@normalframework/applications-sdk");
+const { v5: uuidv5 } = require("uuid");
+
+const batch_size = 500;
+const NAMESPACE = "fe927c12-7f2f-11ee-a65f-af8737c274cc"
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+var token = ""
+
+/**
+ * Invoke hook function
+ * @param {NormalSdk.InvokeParams} params
+ * @returns {NormalSdk.InvokeResult}
+ */
+module.exports = async ({sdk, config, points}) => {
+  http = sdk.http;
+
+  if (!config.username || !config.password || !config.baseUrl) {
+    return NormalSdk.InvokeError("missing username, password, or base url")
+  }
+  config.baseUrl = config.baseUrl.replace(/\/+$/gm, '')
+  console.log(config) 
+  if (token == "") {
+    try{
+      const { data } = await http.post(config.baseUrl + "/token", {
+        grant_type: "password",
+        username: config.username,
+        password: config.password,
+        }, {
+         headers: {
+           'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          timeout: 15000,
+    });
+        token=data.access_token 
+    console.log(data)
+
+    } catch (e) {
+        console.log("catch", e)
+        throw e 
+    }
+  }
+  console.log(token)
+
+  let object_ids = []
+  for (let i = 0; i < points.length; i++) {
+    if (points[i].attrs["objectId"]) {
+        object_ids.push(points[i].attrs["objectId"])
+    }
+  }
+
+  let values = await http.post(config.baseUrl+"/values", object_ids, { headers: {
+        "authorization": "Bearer " + token
+      },
+        timeout: 15000,
+
+  })
+
+  for (let i = 0; i < values.data.length; i ++) {
+    let val = values.data[i].Value
+    if (!val.QualityGood) {
+        continue
+    }
+    var ts, real
+    try{
+        ts = Date.parse(val.Timestamp)
+        real = parseFloat(val.Value)
+    } catch {
+        continue
+    }
+    let uuid = uuidv5(values.data[i].ObjectId, NAMESPACE)
+    console.log(uuid, values.data[i].ObjectId, ts, real)
+
+    let res = await sdk.http.post("http://localhost:8080/api/v1/point/data", {
+        uuid: uuid,
+        values: [
+            {
+                ts: val.Timestamp,
+                real: real,
+            }
+        ]
+    })
+    console.log(res)
+  }
+}
